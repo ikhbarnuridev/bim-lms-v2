b0f85dd0167e05b673c3e9735ab773989d02d315
Ikhbar Nurinsani <ikhbarnuri.dev@gmail.com>
2025-01-04 11:39:25 +0700

    feat: menambahkan informasi pada user guide

--- resources/views/user-guide.blade.php
+++ resources/views/user-guide.blade.php
@@ -6,6 +6,11 @@
             <div class="row row-gap-3">
                 <div class="col-12">
                     <x-section.app.page-title title="{{ $title }}"/>
+
+                    <x-section.info
+                        title="Informasi belum tersedia"
+                        message="Halaman ini sedang dalam tahap pengembangan, harap periksa kembali nanti"
+                    />
                 </div>
             </div>
         </div>
