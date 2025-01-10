3540eecfb8bb9bf570bf2b0a94cfb11720e4945c
Ikhbar Nurinsani <ikhbarnuri.dev@gmail.com>
2025-01-10 19:19:11 +0700

    fix: menghapus tiny mce

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import './bootstrap';
 // Vendors
 import './vendors/simplebar.min.js';
 import * as coreui from '@coreui/coreui'
-import 'tinymce/tinymce.min.js'
 
 // Utilities
 import './utilities/reset-form.js';
